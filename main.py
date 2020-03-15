7e06d85d38a5ef6eb1522a719030b9e80127c09c
Outvi V <git@outv.im>
2020-03-15 17:33:20 +0800

    feat: read student submissions

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import config
 import uuid
 import json
 import os
+import yaml
+import tarfile
+import tempfile
 from collections import defaultdict
 
 ph = PasswordHasher()
@@ -15,6 +18,23 @@ DB = {
     "sessions": [],
     "grading_students": {}
 }
+
+
+class Student:
+    def __init__(self, student_id):
+        filename = os.path.join(config.SUBMISSION_DIR, student_id, "init.yml")
+        try:
+            with open(filename) as fp:
+                content = yaml.load(fp, Loader=yaml.BaseLoader)
+        except Exception as es:
+            print("Failed reading {}: {}".format(student_id, es))
+            self.valid = False
+            return
+        self.student_id: str = student_id
+        self.build_shell: str = content["build_shell"] or ""
+        self.run_shell: str = content["run_shell"] or ""
+        self.notification: str = content["notification"] or ""
+        self.valid = True
 
 
 def savedata():
@@ -27,7 +47,8 @@ def loaddata():
     client_config = json.load(open("client_config.json"))
     DB = db
     DB["client_config"] = client_config
-    DB["grading_students"] = readSubmissions(config.SUBMISSION_DIR)
+    for student_id in readSubmissions(config.SUBMISSION_DIR):
+        DB["grading_students"][student_id] = False
     print("Searching dir {}, {} submissions found.".format(
         config.SUBMISSION_DIR, len(DB["grading_students"])))
 
@@ -36,6 +57,14 @@ def readSubmissions(direc):
     for (dirpath, dirnames, _) in os.walk(direc):
         if dirpath == direc:
             return dirnames
+
+
+def get_tar_result(file, name):
+    f = tempfile.NamedTemporaryFile()
+    tar = tarfile.open(f.name, "w")
+    tar.add(file, name)
+    tar.close()
+    return f
 
 
 class PublicRes:
@@ -57,6 +86,14 @@ def valid_login(req):
     key = req.auth
     key_info = key.split(" ")
     return key_info[0] == "Bearer" and key_info[1] in DB["sessions"]
+
+
+def read_next_ungraded_student():
+    for _, id in enumerate(DB["grading_students"]):
+        if DB["grading_students"][id] == False:
+            print("fs")
+            return Student(id)
+    return None
 
 
 # ---- /register ----
@@ -122,6 +159,59 @@ class ImageRes(PublicRes):
 
     on_get = on_post
 
+# ---- /next ----
+@falcon.before(Authorized)
+class NextRes(PublicRes):
+    def on_get(self, req, resp):
+        student: Student = read_next_ungraded_student()
+        if student == None:
+            resp.status = falcon.HTTP_NOT_FOUND
+            resp.media = {
+                "failure": "No ungraded students",
+                "student": {}
+            }
+            return
+        resp.media = {
+            "failure": "",
+            "student": student.__dict__
+        }
+        DB["grading_students"][student.student_id] = True
+
+    on_post = on_get
+
+# ---- /student/{id} ----
+@falcon.before(Authorized)
+class StudentRes(PublicRes):
+    def on_get(self, req, resp, id, action):
+        student: Student = Student(id)
+        if not student.valid:
+            resp.status = falcon.HTTP_NOT_FOUND
+            resp.media = {
+                "failure": "Student not found"
+            }
+            return
+        if action == "tar":
+            self.return_tar(student, resp)
+        elif action == "info":
+            self.return_info(student, resp)
+        else:
+            resp.status = falcon.HTTP_BAD_GATEWAY
+
+    @staticmethod
+    def return_tar(student, resp):
+        resp.stream = get_tar_result(os.path.join(
+            config.SUBMISSION_DIR, student.student_id),
+            student.student_id)
+        resp.content_type = "application/x-tar"
+
+    @staticmethod
+    def return_info(student: Student, resp):
+        retn = student.__dict__
+        retn["graded"] = DB["grading_students"][student.student_id]
+        resp.media = retn
+
+    on_post = on_get
+
 
 def SigintHandler(signal_received, frame):
     print("Saving data to db...")
@@ -139,4 +229,6 @@ api.add_route("/register", AuthRes())
 api.add_route("/revoke", RevokeRes())
 api.add_route("/config", ConfigRes())
 api.add_route("/image.tar", ImageRes())
+api.add_route("/next", NextRes())
+api.add_route("/student/{id}/{action}", StudentRes())
 api.add_route("/", InfoRes())
